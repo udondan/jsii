045f1e68d60b18749ff2b6823fa8ba7f6643039c
Mitchell Valine <mitchellvaline@yahoo.com>
2020-05-05 16:14:21 -0700

    fix: dependency submodule type resolution

--- packages/jsii-calc/lib/submodules.ts
+++ packages/jsii-calc/lib/submodules.ts
@@ -1,4 +1,5 @@
 import { submodule } from '@scope/jsii-calc-lib';
+import { IReflectable, ReflectableEntry, Reflector } from '@scope/jsii-calc-lib/lib/submodule';
 
 /**
  * Ensures submodule-imported types from dependencies can be used correctly.
@@ -9,6 +10,17 @@ export class UpcasingReflectable implements submodule.IReflectable {
   public constructor(private readonly delegate: Record<string, unknown>) { }
 
   public get entries(): submodule.ReflectableEntry[] {
+      return Object.entries(this.delegate)
+          .map(([key, value]) => ({ key: key.toLocaleUpperCase(), value }));
+  }
+}
+
+export class UpcasingReflectableNamed implements IReflectable {
+  public static readonly reflector = new Reflector();
+
+  public constructor(private readonly delegate: Record<string, unknown>) { }
+
+  public get entries(): ReflectableEntry[] {
       return Object.entries(this.delegate)
           .map(([key, value]) => ({ key: key.toLocaleUpperCase(), value }));
   }
